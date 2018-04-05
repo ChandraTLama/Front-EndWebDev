92aff700b7b7fdec4dc5ed972e50c54a3e4764ad
ChandraTLama <clama@unomaha.edu>
2018-04-05 06:10:29 -0500

    murder mystery quiz for javascript/conditional

---
+++ murder-mystery-conditional-javascript/murder_mystery.js
@@ -0,0 +1,38 @@
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "billiards room";
+var suspect = "Mrs. Sparr";
+
+var weapon = "";
+var solved = false;
+
+if (room === "ballroom") {
+    weapon = "poison";
+    if(suspect === "Mr. Kalehoff"){
+         solved = true;
+     }
+    
+} else if (room === "gallery") {
+    weapon = "trophy";
+    if(suspect === "Ms. Van Cleve"){
+         solved = true;
+     }
+    
+} else if (room === "billiards room") {
+    weapon = "pool stick";
+    if(suspect === "Mrs. Sparr"){
+         solved = true;
+     }
+} else if (room === "dining room") {
+    weapon = "knife";
+    if(suspect === "Mr. Parkes"){
+         solved = true;
+     }
+}
+
+if (solved) {
+    console.log(suspect + " " + "did it in the" +" " + room +" " +"with the"+" " + weapon+ "!");
+}
